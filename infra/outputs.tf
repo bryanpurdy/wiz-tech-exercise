87af2ea29beb308e8d3243d00ff11348277241d2
Bryan Purdy <bryanpurdy@Bryans-MacBook-Pro.local>
2025-09-28 11:00:36 -0700

    Fix EKS cluster data source dependency issue

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,8 +1,3 @@
-# Fetch cluster details directly
-data "aws_eks_cluster" "this" {
-  name = module.eks.cluster_name
-}
-
 # Mongo EC2 Private IP
 output "mongo_private_ip_a" {
   value = aws_instance.mongo.private_ip
@@ -25,17 +20,17 @@ output "eks_cluster_endpoint" {
 
 # EKS Cluster Endpoint Public Access
 output "eks_cluster_endpoint_public_access" {
-  value = data.aws_eks_cluster.this.vpc_config[0].endpoint_public_access
+  value = module.eks.cluster_endpoint_public_access
 }
 
 # EKS Cluster Endpoint Private Access
 output "eks_cluster_endpoint_private_access" {
-  value = data.aws_eks_cluster.this.vpc_config[0].endpoint_private_access
+  value = module.eks.cluster_endpoint_private_access
 }
 
 # Public cidr
 output "eks_cluster_public_access_cidrs" {
-  value = data.aws_eks_cluster.this.vpc_config[0].public_access_cidrs
+  value = module.eks.cluster_endpoint_public_access_cidrs
 }
 
 # Mongo Instance ID
